d29ef64727c8db7855e1c69b0bf48d298fa99438
Quentin Bernet <quentin.bernet@bluewin.ch>
2025-12-09 08:48:01 +0000

    Add some types to pre-existing codebase

--- vyper/semantics/analysis/module.py
+++ vyper/semantics/analysis/module.py
@@ -738,7 +738,7 @@ class ModuleAnalyzer(VyperNodeVisitorBase):
     def visit_ImportFrom(self, node):
         self._add_import(node)
 
-    def _add_import(self, node: vy_ast.VyperNode) -> None:
+    def _add_import(self, node: vy_ast.Import | vy_ast.ImportFrom) -> None:
         for import_info in node._metadata["import_infos"]:
             # similar structure to import analyzer
             module_info = self._load_import(import_info)
@@ -747,7 +747,7 @@ class ModuleAnalyzer(VyperNodeVisitorBase):
 
             Namespace.builder_context.get()[import_info.alias] = module_info
 
-    def _load_import(self, import_info: ImportInfo) -> Any:
+    def _load_import(self, import_info: ImportInfo) -> ModuleInfo | InterfaceT:
         path = import_info.compiler_input.path
 
         if path.suffix == ".vy":
